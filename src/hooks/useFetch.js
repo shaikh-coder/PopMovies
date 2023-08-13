b0c9d8c9587bb99c7897122d402b62205053de3d
md hammad <hammadwoodbine@gmail.com>
2023-08-13 10:21:54 +0530

    fixed error

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 export const useFetch = (apiPath, queryTerm="") => {
     const [data,setData]= useState([]);
-    const url = `http://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`
 
     useEffect(()=>{
         async function fetchMovies(){
